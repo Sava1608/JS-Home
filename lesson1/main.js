5b19d3703b91490a5b9e9b2689ea0fb6c93ddb8a
Sava1608 <vtoper43@gmail.com>
2024-08-05 21:00:38 +0300

    HomeWork1

---
+++ lesson1/main.js
@@ -0,0 +1,57 @@
+// Перше завдання
+let string1= "Hello";
+console.log(string1);
+let string2= "Owu";
+console.log(string2);
+let string3= "com";
+console.log(string3);
+let string4= "ua";
+console.log(string4);
+let fullstring=`${string1} ${string2} ${string3} ${string4}`;
+console.log(fullstring);
+
+let number1= 1;
+console.log(number1);
+let number2= 10;
+console.log(number2);
+let number3= -999;
+console.log(number3);
+let number4= 123;
+console.log(number4);
+
+const constanta=3.14;
+console.log(constanta);
+
+let number5= 2.7;
+console.log(number5);
+let number6= 16;
+console.log(number6);
+
+let bool1= true;
+console.log(bool1);
+let bool2= false;
+console.log(bool2);
+
+// Друге завдання
+
+let firstName ='Baran';
+let middleName='Yaroslav';
+let lastName='Orestovich';
+let person=`${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+// Третє завдання
+
+let a=100;
+console.log(typeof a);
+let b='100';
+console.log(typeof b);
+let c=true;
+console.log(typeof c);
+
+// Промпти
+
+let Name = prompt()
+let fatherName = prompt()
+let age = prompt()
+console.log(`${Name} ${fatherName} ${age}`)
